cd5411fcad7a0686b64555cabb072eeeaf7f2e0d
Shubham Jaybhaye <shubhamj7997@gmail.com>
2020-06-09 22:17:39 +0530

    Fix sample script

--- SampleScript.kts
+++ SampleScript.kts
@@ -3,8 +3,6 @@ import esw.ocs.dsl.core.script
 script {
     println("loading Script")
 
-    loadScript(ReusableScript)
-
     onSetup("command-1") { x ->
         println("Command received" + x)
     }
